0930dd990142e941e844e94beefc4e3cd75bd7e9
s5teixeira <steixeira1555@gmail.com>
2023-04-24 00:09:09 -0400

    added init function

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+class Item:
+    def __init__(self, name: str, price: float, item_type: str):
+        self.name = name
+        self.price = price
+        self.item_type = item_type
+
+
+
+
+
+
 
 
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+  #  main()
 
 
