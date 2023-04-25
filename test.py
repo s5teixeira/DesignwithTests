10737fa85ad6e6db5ea3e37c938d125d6a2be3e4
s5teixeira <steixeira1555@gmail.com>
2023-04-25 18:20:49 -0400

    more test functions

--- test.py
+++ test.py
@@ -1,7 +1,21 @@
 from production import Checkout, Item
 
-def test_NJ_WIC():
+def test_NJ_Wic():
     checkout = Checkout()
     items = [Item('Apples', 2.5, 'Wic Eligible food')]
     assert checkout.calculate_total('NJ', items) == 2.5
 
+def test_PA_Clothing():
+    checkout = Checkout()
+    items = [Item('T-shirt', 10.0, 'Clothing')]
+    assert checkout.calculate_total('PA', items) == 10.0
+
+def test_DE_Everything_else():
+    checkout = Checkout()
+    items = [Item('Book', 15.0, 'Books'), Item('Book2', 25.0, 'Books')]
+    assert checkout.calculate_total('DE', items) == 40.0
+
+def test_NJ_Fur():
+    checkout = Checkout()
+    items = [Item('Fur coat', 500.0, 'Clothing')]
+    assert checkout.calculate_total('NJ', items) == 533.0
